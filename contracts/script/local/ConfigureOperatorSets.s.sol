ba150e4ba8bb8a037d0bad4a4db574bb8da01689
rubydusa <rubydusa@protonmail.com>
2025-11-23 20:09:10 +0200

    fix: match `ConfigureOperatorSets` to use only one `OperatorSet`

--- contracts/script/local/ConfigureOperatorSets.s.sol
+++ contracts/script/local/ConfigureOperatorSets.s.sol
@@ -24,24 +24,18 @@ contract ConfigureOperatorSets is Script {
 
         vm.startBroadcast(avsPrivateKey);
 
-        // Configure aggregator operator set (ID 0) for ECDSA
+        // Configure aggregator operator set (ID 0) for BN254
         OperatorSet memory operatorSet0 = OperatorSet({avs: avsAddress, id: 0});
 
-        console.log("Configuring operator set 0 (aggregator) for ECDSA...");
-        KEY_REGISTRAR.configureOperatorSet(operatorSet0, IKeyRegistrarTypes.CurveType.ECDSA);
-
-        // Configure executor operator set (ID 1) for ECDSA
-        OperatorSet memory operatorSet1 = OperatorSet({avs: avsAddress, id: 1});
-
-        console.log("Configuring operator set 1 (executor) for ECDSA...");
-        KEY_REGISTRAR.configureOperatorSet(operatorSet1, IKeyRegistrarTypes.CurveType.ECDSA);
+        console.log("Configuring operator set 0 (aggregator) for BN254...");
+        KEY_REGISTRAR.configureOperatorSet(operatorSet0, IKeyRegistrarTypes.CurveType.BN254);
 
         vm.stopBroadcast();
 
         console.log("Successfully configured operator sets");
 
         // Write configuration results to JSON file
-        string memory json = "config";
+        string memory json = "configOperatorSets";
         vm.serializeAddress(json, "avs", avsAddress);
         vm.serializeAddress(json, "keyRegistrar", keyRegistrarAddress);
         vm.writeJson(json, outputPath);
